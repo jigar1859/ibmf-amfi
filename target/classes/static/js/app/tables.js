82a1042c8551aea2188937092c3ca488dfbe789f
Jigar Thakkar <jigar1859@gmail.com>
2019-12-14 21:52:28 +0530

    Target Deleted

--- target/classes/static/js/app/tables.js
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-var carouselItem = document.getElementById("carouselItem");
-var ibmfGuiltFund = document.getElementById("ibmf-guiltfund");
-console.log("carouselItem"+carouselItem);
-console.log("ibmfGuiltFund"+ibmfGuiltFund);
-carouselItem.appendChild(ibmfGuiltFund);
